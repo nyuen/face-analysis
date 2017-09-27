e3d3083553a59266c02b3d81631ecc9f3ba9b545
olileger <olileger@microsoft.com>
2017-09-27 17:55:09 +0200

    First commit from AzFunc

---
+++ analyze/index.js
@@ -0,0 +1,113 @@
+/**
+ * Includes.
+ */
+const r = require("request");
+
+
+/**
+ * Entry point.
+ */
+module.exports = function (context, req)
+{
+    AnalyzeEmotion({ context : context, data : req.body }, {}, AnalyzeFace);
+};
+
+
+/**
+ * Call the CS Emotion API.
+ */
+function AnalyzeEmotion(dataIn, dataOut, next)
+{
+    CallCSApi("https://westus.api.cognitive.microsoft.com/emotion/v1.0/recognize",
+              process.env.EMOTIONAPI_KEY,
+              dataIn.data,
+              function (err, res, body)
+              {
+                  let emotion = "";
+                  if (res.statusCode == 200)
+                  {
+                      let scores = JSON.parse(body)[0].scores
+                      for (let p in scores)
+                      {
+                          if (scores[p] >= 0.25)
+                          {
+                              emotion += p + " ";
+                          }
+                      }
+                  }
+                  dataOut.emotion = emotion;
+                  next(dataIn, dataOut, ReturnResponse);
+              });
+}
+
+
+/**
+ * Call the CS Face API.
+ */
+function AnalyzeFace(dataIn, dataOut, next)
+{
+    CallCSApi("https://westus.api.cognitive.microsoft.com/face/v1.0/detect?returnFaceAttributes=age,gender,smile,facialHair,glasses",
+              process.env.FACEAPI_KEY,
+              dataIn.data,
+              function (err, res, body)
+              {
+                let attributes = {};
+                if (res.statusCode == 200)
+                {
+                    attributes = JSON.parse(body)[0].faceAttributes
+                    if (attributes.smile <= 0.30)
+                    {
+                        attributes.smile = "No";
+                    }
+                    else if (attributes.smile > 0.30 && attributes.smile <= 0.75)
+                    {
+                        attributes.smile = "Maybe";
+                    }
+                    else
+                    {
+                        attributes.smile = "Yes !!";
+                    }
+                }
+                dataOut.attributes = attributes;
+                next(dataIn, dataOut, null);
+              });
+}
+
+
+/**
+ * Return the answer to the client.
+ */
+function ReturnResponse(dataIn, dataOut)
+{
+    dataIn.context.res = {};
+    dataIn.context.res.status = 200;
+    dataIn.context.res.headers = { "content-type" : "application/json" };
+    dataIn.context.res.body = JSON.stringify(dataOut);
+    dataIn.context.done();
+}
+
+
+/**
+ * Wrapper function to call Azure Cognitive Services easily.
+ */
+function CallCSApi(url, key, data, callback)
+{
+    r({
+        url : url,
+        headers :
+        {
+            "Content-Type" : data.url ? "application/json": "application/octet-stream",
+            "Ocp-Apim-Subscription-Key" : key
+        },
+        method : "POST",
+        body : data.url ? JSON.stringify(data): data
+      },
+      callback);
+}
+
+
+// Dev mode.
+if (process.env.NODE_ENV == "dev")
+{
+    module.exports({ done : function() { console.log("done() called"); } }, { body : { url : "http://www.afr.com/content/dam/images/g/u/9/f/4/6/image.related.afrArticleLead.620x350.gtf1i7.png/1486690175461.jpg" } });
+}
---
+++ spa/index.js
@@ -0,0 +1,19 @@
+const app = require("express")();
+const fs = require("fs");
+const path = require("path");
+
+app.get("/api/spa", function(req, res)
+{
+    // Read the SPA file.
+    let page = fs.readFileSync(path.join(__dirname, "/form.html"));
+
+    // Replace the API url.
+    page = String(page).replace("ANALYZEAPI_URL", process.env.ANALYZEAPI_URL);
+    page = page.replace("SENDFEEDBACKAPI_URL", process.env.SENDFEEDBACKAPI_URL);
+
+    // Serve it !
+    res.set("Content-type", "text/html");
+    res.status(200).send(page);
+});
+
+module.exports = require("azure-function-express").createAzureFunctionHandler(app);
