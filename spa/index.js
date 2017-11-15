096501de04ecf49e4291029cd8ffe9197539358a
olileger <olileger@microsoft.com>
2017-11-15 22:52:35 +0100

    Removing Azure Functions dependencies

--- spa/index.js
+++ spa/index.js
@@ -1,9 +1,15 @@
-const app = require("express")();
+/**
+ * Includes.
+ */
 const fs = require("fs");
 const os = require("os");
 const path = require("path");
 
-app.get("/api/spa", function(req, res)
+
+/**
+ * Entry point.
+ */
+module.exports =  function (context, req)
 {
     // Read the SPA file.
     let page = fs.readFileSync(path.join(__dirname, "/form.html"));
@@ -15,9 +21,25 @@ app.get("/api/spa", function(req, res)
     page = String(page).replace("ANALYZEAPI_URL", process.env.ANALYZEAPI_URL);
     page = page.replace("SENDFEEDBACKAPI_URL", process.env.SENDFEEDBACKAPI_URL);
 
-    // Serve it !
-    res.set("Content-type", "text/html");
-    res.status(200).send(page);
-});
+    // Serve the page.
+    context.res.headers = { "content-type" : "text/html" };
+    context.res.status = 200;
+    context.res.body = page;
+    context.done();
+};
 
-module.exports = require("azure-function-express").createAzureFunctionHandler(app);
+
+// Dev mode.
+if (process.env.NODE_ENV == "dev")
+{
+    require("http").createServer(function(req, res)
+    {
+        let context = 
+        {
+            done : function() { console.log("done() called"); },
+            res : res
+        };
+        module.exports(context, req);
+        res.end(res.body);
+    }).listen(8080);
+}
