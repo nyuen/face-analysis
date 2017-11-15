e2ec9b27a4e938e3be06acdfc168bab1d765c630
olileger <olileger@microsoft.com>
2017-11-15 16:32:11 +0100

    Adding hostname

--- spa/index.js
+++ spa/index.js
@@ -1,11 +1,15 @@
 const app = require("express")();
 const fs = require("fs");
+const os = require("os");
 const path = require("path");
 
 app.get("/api/spa", function(req, res)
 {
     // Read the SPA file.
     let page = fs.readFileSync(path.join(__dirname, "/form.html"));
+
+    // Replace the hostname.
+    page = String(page).replace("HOSTNAME", os.hostname());
 
     // Replace the API url.
     page = String(page).replace("ANALYZEAPI_URL", process.env.ANALYZEAPI_URL);
